ded8d3ca4e8d92f24e84e9497e886fa1e4aeaaa5
LegendKi <rmadyd0314@naver.com>
2024-05-31 17:54:46 +0900

    fix: change model hyperparmeter

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def train_model(data):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=10000, learning_rate=0.01, max_depth=10)
+    model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1, max_depth=3)
     model.fit(X_train, y_train)
     
     predictions = model.predict(X_test)
